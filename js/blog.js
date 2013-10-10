aa32d3b996641a997bf4c768212100144abbae0e
varyo <japedo24@gmail.com>
2013-10-10 20:50:23 +0200

    disqus test

--- js/blog.js
+++ js/blog.js
@@ -11,6 +11,10 @@ processJSON = function( data ) {
 
        // the content
 	   $( ".container" ).append( '<div class="sixteen columns"><h3>'+e.Title+'</h3><h4>'+e.Date+' by '+e.Author+'</h4><p>'+e.Content+'</p></div>' );
+
+	   // // disqus
+	   // $( ".container" ).append( '<div id="disqus_thread"></div><script type="text/javascript">var disqus_shortname = "varyo";(function() {var dsq = document.createElement("script"); dsq.type = "text/javascript"; dsq.async = true;dsq.src = "//" + disqus_shortname + ".disqus.com/embed.js";(document.getElementsByTagName("head")[0] || document.getElementsByTagName("body")[0]).appendChild(dsq);})();</script>');
+
 	});
 }
 
