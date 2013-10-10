429294d192742bd640d7b6f1f625161afb2f7c7b
varyo <japedo24@gmail.com>
2013-10-10 20:56:59 +0200

    disqus layout improvments...

--- js/blog.js
+++ js/blog.js
@@ -1,6 +1,6 @@
 processJSON = function( data ) {
 	// title
-	$( ".post-container" ).append( '<div class="sixteen columns"><h1>'+data.Title+'</h1><h5>'+data.Subtitle+'</h5><hr/></div>' );
+	$( "#post-container" ).append( '<div class="sixteen columns"><h1>'+data.Title+'</h1><h5>'+data.Subtitle+'</h5><hr/></div>' );
 
   	// blog posts
 	$.each( data.posts, function( i, e ) {
@@ -10,11 +10,7 @@ processJSON = function( data ) {
 	   $( "head" ).append( '<meta name="keywords" content="'+e.Tags+'">' );
 
        // the content
-	   $( ".post-container" ).append( '<div class="sixteen columns"><h3>'+e.Title+'</h3><h4>'+e.Date+' by '+e.Author+'</h4><p>'+e.Content+'</p></div>' );
-
-	   // // disqus
-	   // $( ".post-container" ).append( '<div id="disqus_thread"></div><script type="text/javascript">var disqus_shortname = "varyo";(function() {var dsq = document.createElement("script"); dsq.type = "text/javascript"; dsq.async = true;dsq.src = "//" + disqus_shortname + ".disqus.com/embed.js";(document.getElementsByTagName("head")[0] || document.getElementsByTagName("body")[0]).appendChild(dsq);})();</script>');
-
+	   $( "#post-container" ).append( '<div class="sixteen columns"><h3>'+e.Title+'</h3><h4>'+e.Date+' by '+e.Author+'</h4><p>'+e.Content+'</p></div>' );
 	});
 }
 
