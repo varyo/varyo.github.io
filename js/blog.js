55363e72df8d3ba38fbd97219604de7079687b4b
varyo <japedo24@gmail.com>
2013-10-10 17:40:38 +0200

    initial commit

---
+++ js/blog.js
@@ -0,0 +1,22 @@
+processJSON = function( data ) {
+	// title
+	$( ".container" ).append( '<div class="sixteen columns"><h1>'+data.Title+'</h1><h5>'+data.Subtitle+'</h5><hr/></div>' );
+
+	// blog posts
+	$.each( data.posts, function( i, e ) {
+		$( ".container" ).append( '<div class="sixteen columns"><h3>'+e.Title+'</h3><h4>'+e.Date+' by '+e.Author+'</h4><p>'+e.Content+'</p></div>' );
+	});
+}
+
+$( document ).ready(function() {	
+	$.getJSON( "./blog/posts.json" )
+  		.done(function( json ) {
+  			// success
+  			processJSON( json );
+  		})
+  		.fail(function( jqxhr, textStatus, error ) {
+  			// fail
+    		var err = textStatus + ", " + error;
+    		console.log( "Request Failed: " + err );
+	});
+});
