e7f477034801248687780ec8a09a93d3cb6dc436
varyo <japedo24@gmail.com>
2013-10-10 18:03:21 +0200

    added html meta stuff

--- js/blog.js
+++ js/blog.js
@@ -2,9 +2,15 @@ processJSON = function( data ) {
 	// title
 	$( ".container" ).append( '<div class="sixteen columns"><h1>'+data.Title+'</h1><h5>'+data.Subtitle+'</h5><hr/></div>' );
 
-	// blog posts
+  	// blog posts
 	$.each( data.posts, function( i, e ) {
-		$( ".container" ).append( '<div class="sixteen columns"><h3>'+e.Title+'</h3><h4>'+e.Date+' by '+e.Author+'</h4><p>'+e.Content+'</p></div>' );
+       // add meta stuff 
+	   $( "head" ).append( '<meta name="author" content="'+e.Author+'">' );
+	   $( "head" ).append( '<meta name="date" content="'+e.Date+'">' );
+	   $( "head" ).append( '<meta name="keywords" content="'+e.Tags+'">' );
+
+       // the content
+	   $( ".container" ).append( '<div class="sixteen columns"><h3>'+e.Title+'</h3><h4>'+e.Date+' by '+e.Author+'</h4><p>'+e.Content+'</p></div>' );
 	});
 }
 
