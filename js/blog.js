16d9d197db28cbe5c22011788ac94fa1ee0e9e19
varyo <japedo24@gmail.com>
2013-10-11 10:24:51 +0200

    added post overview

--- js/blog.js
+++ js/blog.js
@@ -4,26 +4,42 @@ $.processJSON = function( data ) {
 
     // blog posts
     var postId = $.urlParam( "id" );
-    $.each( data.posts, function( i, e ) {
-        // show post x that is called with 'index.html?id=x'
-        if( postId == i ) {
-           // add meta stuff 
-           $( "head" ).append( '<meta name="author" content="'+e.Author+'">' );
-           $( "head" ).append( '<meta name="date" content="'+e.Date+'">' );
-           $( "head" ).append( '<meta name="keywords" content="'+e.Tags+'">' );
+    if( postId < 0 ) {
+        // iterate over the posts and put them in the table
+        $.each( data.posts, function( i, e ) {
+            $( "#overview" ).prepend( '<tr><td><a href="index.html?id='+i+'#disqus_thread" data-disqus-identifier="'+i+'">'+e.Title+'</a></td><td>'+e.Date+'</td></tr>' );
+        });   
+    } else {
 
-           // the content
-           $( "#post-container" ).append( '<div class="sixteen columns"><h3>'+e.Title+'</h3><h4>'+e.Date+' by '+e.Author+'</h4><p>'+e.Content+'</p></div>' );
+        $.each( data.posts, function( i, e ) {
+            // show post x that is called with 'index.html?id=x'
+            if( postId == i ) {
+                // if we found a post, hide the overview container
+                $( "#overview-container" ).hide();
 
-           // load disqus
-           $( "#disqus" ).append( '<div class="sixteen columns"><div id="disqus_thread"></div><script type="text/javascript">var disqus_shortname = "varyo"; disqus_identifier = "'+postId+'";(function() {var dsq = document.createElement("script"); dsq.type = "text/javascript"; dsq.async = true;dsq.src = "//" + disqus_shortname + ".disqus.com/embed.js";(document.getElementsByTagName("head")[0] || document.getElementsByTagName("body")[0]).appendChild(dsq);})();</script></div>' );
-        }
-    });
+                // add meta stuff 
+                $( "head" ).append( '<meta name="author" content="'+e.Author+'">' );
+                $( "head" ).append( '<meta name="date" content="'+e.Date+'">' );
+                $( "head" ).append( '<meta name="keywords" content="'+e.Tags+'">' );
+
+                // the content
+                $( "#post-container" ).append( '<div class="sixteen columns"><h3>'+e.Title+'</h3><h4>'+e.Date+' by '+e.Author+'</h4><p>'+e.Content+'</p></div>' );
+
+                // back button
+                $( "#post-container" ).append( '<div class="sixteen columns"><p><a href="index.html">Back</a></p></div>');
+
+                // load disqus
+                $( "#disqus" ).append( '<div class="sixteen columns"><div id="disqus_thread"></div><script type="text/javascript">var disqus_shortname = "varyo"; disqus_identifier = "'+postId+'";(function() {var dsq = document.createElement("script"); dsq.type = "text/javascript"; dsq.async = true;dsq.src = "//" + disqus_shortname + ".disqus.com/embed.js";(document.getElementsByTagName("head")[0] || document.getElementsByTagName("body")[0]).appendChild(dsq);})();</script></div>' );
+            }
+        });
+
+    }
 }
 
 $.urlParam = function( name ) {
     var results = new RegExp('[\\?&amp;]' + name + '=([^&amp;#]*)').exec(window.location.href);
-    return results[1] || 0;
+    if( !results ) return -1;
+    return results[1];
 }
 
 $( document ).ready(function() {    
