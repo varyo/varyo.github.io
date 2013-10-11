6e8ffdbc378c13508856f80d2061d6f0dc68e350
varyo <japedo24@gmail.com>
2013-10-11 09:30:23 +0200

    javascript comment deleted that caused an error

--- js/blog.js
+++ js/blog.js
@@ -16,7 +16,7 @@ $.processJSON = function( data ) {
            $( "#post-container" ).append( '<div class="sixteen columns"><h3>'+e.Title+'</h3><h4>'+e.Date+' by '+e.Author+'</h4><p>'+e.Content+'</p></div>' );
 
            // load disqus
-           $( "#disqus" ).append( '<div class="sixteen columns"><div id="disqus_thread"></div><script type="text/javascript">var disqus_shortname = "varyo"; // required: replace example with your forum shortnamevar disqus_identifier = "'+postId+'";(function() {var dsq = document.createElement("script"); dsq.type = "text/javascript"; dsq.async = true;dsq.src = "//" + disqus_shortname + ".disqus.com/embed.js";(document.getElementsByTagName("head")[0] || document.getElementsByTagName("body")[0]).appendChild(dsq);})();</script></div>' );
+           $( "#disqus" ).append( '<div class="sixteen columns"><div id="disqus_thread"></div><script type="text/javascript">var disqus_shortname = "varyo"; disqus_identifier = "'+postId+'";(function() {var dsq = document.createElement("script"); dsq.type = "text/javascript"; dsq.async = true;dsq.src = "//" + disqus_shortname + ".disqus.com/embed.js";(document.getElementsByTagName("head")[0] || document.getElementsByTagName("body")[0]).appendChild(dsq);})();</script></div>' );
         }
     });
 }
